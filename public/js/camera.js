faf7eb8aff977d3dd550c2007b1cdfb50ff28bee
Andy Xia <anxia@ucsd.edu>
2018-03-01 13:59:19 -0800

    api lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,6 +1,6 @@
 function initCamera() {
   // Uncomment and fill in the correct selectors below.
-  // capture($('<Your code here>'),
+//capture($('<Your code here>'),
   //        $('<Your code here>'),
   //        $('<Your code here>'));
     capture($('#camera-video'),
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,8 +1,8 @@
-// 'use strict';
-
+// 'use strict';  
 // Call this function when the page loads (the "ready" event)
+
 $(document).ready(function() {
-  initializePage();
+	initializePage();
 })
 
 /*
@@ -12,15 +12,38 @@ function initializePage() {
 	console.log("Page ready");
  	initCamera();
  	initGestures();
- 	initRSVPForm();
+ 	//initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			// get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+			// show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+  $('#rsvpForm').submit(function(e){
+
+  	e.preventDefault();
+  	console.log("submitting form...")
+  	var rsvpEmail = $('#rsvpEmail').val();
+
+  	$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+  });
+
+  function postCallback(res) {
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val('');
+  }
 }
